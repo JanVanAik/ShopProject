c67a271cfd9787a45d2e352ed42e00b37fb2fe38
JanVanAik <ar.orlovskiy@gmail.com>
2024-04-20 00:42:58 +0300

    Initial commit

--- basket/models.py
+++ basket/models.py
@@ -1,4 +1,6 @@
 from products.models import Product
+from django.db import models
+from users.models import User
 
 # Create your models here.
 class Basket(models.Model):
--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
+from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from django.utils.timezone import now
 from datetime import timedelta
-from django.db.models.signals import post_save
+
 
 
 # Create your models here.
